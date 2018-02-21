8c9c41989597a3d54eff7942ce78c20b46cc4d29
Frank Reno <freno@sumologic.com>
2018-02-21 10:21:54 -0700

    remove sched module, add logging, converting to a cronjob

--- extract-data.py
+++ extract-data.py
@@ -7,48 +7,39 @@ import logging
 import json
 import requests
 import os
-import sched
 import sys
-import time
 
-s = sched.scheduler(time.time, time.sleep)
+logging.basicConfig(level="INFO", format="%(asctime)s [level=%(levelname)s] [line=%(lineno)d]: %(message)s")
+log = logging.getLogger(__name__)
+
 if os.environ.get('SUMO_HTTP_URL') is not None:
     if os.environ.get('SUMO_HTTP_URL'):
         SUMO_COLLECTOR_URL = os.environ.get('SUMO_HTTP_URL')
     else:
-        logging.error("Collector was defined but is Empty")
-        sys.exit(os.EX_CONFIG) 
+        log.error("Collector was defined but is Empty")
+        sys.exit(os.EX_CONFIG)
 else:
-    logging.error("NO Collector was Defined")
+    log.error("NO Collector was Defined")
     sys.exit(os.EX_CONFIG)
 
 if os.environ.get('K8S_API_URL') is not None:
     if os.environ.get('K8S_API_URL'):
         K8S_API_URL = os.environ.get('K8S_API_URL')
     else:
-        logging.error("K8s_API_URL was defined but is Empty")
+        log.error("K8s_API_URL was defined but is Empty")
         sys.exit(os.EX_CONFIG)
 else:
-    logging.error("No Kubernetes API defined")
+    log.error("No Kubernetes API defined")
     sys.exit(os.EX_CONFIG)
 
-''' Default to 60 seconds'''
-RUN_EVERYTIME = int(os.getenv('RUN_TIME', 60))
+log.info("getting data for nodes")
+nodes = requests.get(url="{}/api/v1/nodes".format(K8S_API_URL)).json()
+for node in nodes["items"]:
+    log.info("pushing to sumo")
+    requests.post(url=SUMO_COLLECTOR_URL, data=json.dumps(node))
 
-
-def fetch_push():
-    logging.info("getting data for nodes")
-    nodes = requests.get(url="{}/api/v1/nodes".format(K8S_API_URL)).json()
-    for node in nodes["items"]:
-        logging.info("pushing to sumo")
-        requests.post(url=SUMO_COLLECTOR_URL, data=json.dumps(node))
-
-    logging.info("getting data for nodes")
-    pods = requests.get(url="{}/api/v1/pods".format(K8S_API_URL)).json()
-    for pod in pods["items"]:
-        logging.info("pushing to sumo")
-        requests.post(url=SUMO_COLLECTOR_URL, data=json.dumps(pod))
-
-
-s.enter(RUN_EVERYTIME, 1, fetch_push, ())
-s.run()
+log.info("getting data for nodes")
+pods = requests.get(url="{}/api/v1/pods".format(K8S_API_URL)).json()
+for pod in pods["items"]:
+    log.info("pushing to sumo")
+    requests.post(url=SUMO_COLLECTOR_URL, data=json.dumps(pod))
